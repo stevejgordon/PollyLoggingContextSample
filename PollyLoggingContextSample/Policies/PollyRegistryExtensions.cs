5915edc212b3ee79aa69c6b07127769565fd131e
Steve Gordon <sgordon@hotmail.co.uk>
2018-10-03 13:20:39 +0100

    Utilising PolicyKey

--- PollyLoggingContextSample/Policies/PollyRegistryExtensions.cs
+++ PollyLoggingContextSample/Policies/PollyRegistryExtensions.cs
@@ -15,18 +15,19 @@ namespace PollyLoggingContextSample.Policies
                 .OrResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
                 .WaitAndRetryAsync(3, retryCount => TimeSpan.FromSeconds(10), (result, timeSpan, retryCount, context) =>
                 {
-                    if (context.TryGetLogger(out var logger))
+                    if (!context.TryGetLogger(out var logger)) return;
+
+                    if (result.Exception != null)
                     {
-                        if (result.Exception != null)
-                        {
-                            logger.LogError(result.Exception, "An exception occurred on {RetryAttempt}", retryCount);
-                        }
-                        else
-                        {
-                            logger.LogError("A non success code {StatusCode} was received on {RetryAttempt}", (int)result.Result.StatusCode, retryCount);
-                        }
+                        logger.LogError(result.Exception, "An exception occurred on retry {RetryAttempt} for {PolicyKey}", retryCount, context.PolicyKey);
                     }
-                });
+                    else
+                    {
+                        logger.LogError("A non success code {StatusCode} was received on retry {RetryAttempt} for {PolicyKey}", 
+                            (int)result.Result.StatusCode, retryCount, context.PolicyKey);
+                    }
+                })
+                .WithPolicyKey(PolicyNames.BasicRetry);
 
             policyRegistry.Add(PolicyNames.BasicRetry, retryPolicy);
 
